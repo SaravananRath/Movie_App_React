7a5ca5a2f13af422653a6b998bbc918c7d3add8e
SaravananRath <saravananr95m16@gmail.com>
2018-06-25 17:26:59 +0530

    Axios movie search implemented

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-
-axios.get('https://api.themoviedb.org/3/movie/76341?api_key=4b6b3b4b55597a6d97748a53f91b1d20')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+// https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=The+Mummy
+// https://api.themoviedb.org/3/movie/76341?api_key=4b6b3b4b55597a6d97748a53f91b1d20
+// axios.get('https://api.themoviedb.org/3/search/movie?api_key=4b6b3b4b55597a6d97748a53f91b1d20&query=The+Mummy')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      search:''
+    }
+  }
+  handleChange = (e) =>{
+    this.setState({search:e.target.value})
+  }
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    axios.get('https://api.themoviedb.org/3/search/movie', {
+      params: {
+        api_key:'4b6b3b4b55597a6d97748a53f91b1d20',
+        query:this.state.search
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   render() {
     return (
       <div className="App">
-        
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Hollywood Movie:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Search</button>
+        </form>
       </div>
     );
   }
